d277aa6b049de4f38afcd50dace3bd83b24ae85a
meitu <pl@meitu.com>
2017-09-04 10:37:45 +0800

    add code

--- build/dev-client.js
+++ build/dev-client.js
@@ -1,4 +1,5 @@
 // http://www.cnblogs.com/donglegend/p/5821092.html
+// https://github.com/leecade/koa-webpack-middleware
 /* eslint-disable */
 require('eventsource-polyfill')
 var hotClient = require('webpack-hot-middleware/client?noInfo=true&reload=true')
