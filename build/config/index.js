a15d7a9d3a295ba510677a5d36923cb4acefe611
meitu <pl@meitu.com>
2017-09-30 17:22:08 +0800

    add: build code

--- build/config/index.js
+++ build/config/index.js
@@ -18,8 +18,8 @@ module.exports = {
   },
   build: {
     env: { NODE_ENV: '"production"' },
-    index: path.resolve(__dirname, '../dist/index.html'),
-    assetsRoot: path.resolve(__dirname, '../dist'),
+    index: path.resolve(__dirname, '../../dist/index.html'),
+    assetsRoot: path.resolve(__dirname, '../../dist'),
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
     productionSourceMap: true,
